4a1e84ba0476d9720542b7ef490eb1ce173b0081
jeet1desai <jeetdesai995@gmail.com>
2024-06-18 16:07:42 +0530

    auto complete react and js

--- src/AutoCompleteSearch/index.jsx
+++ src/AutoCompleteSearch/index.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const AutoCompleteSearch = () => {
-  return <div></div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [isResultVisible, setResultVisible] = useState(false);
+  const [cache, setCache] = useState({});
+
+  const fetchData = async () => {
+    if (cache[search]) {
+      console.log("cache");
+      setSearchResult(cache[search]);
+    } else {
+      console.log("API");
+      const data = await fetch(`https://www.google.com/complete/search?client=firefox&q=${search}`);
+      const json = await data.json();
+
+      cache[search] = json[1];
+      setSearchResult(json[1]);
+    }
+  };
+
+  const handleSearch = (value) => {
+    setSearch(value);
+  };
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      fetchData();
+    }, [500]);
+    return () => clearTimeout(timer);
+  }, [search]);
+
+  console.log(cache);
+
+  return (
+    <div className="auto-complete">
+      <input
+        onBlur={() => setResultVisible(false)}
+        onFocus={() => setResultVisible(true)}
+        onChange={(e) => handleSearch(e.target.value)}
+        type="text"
+        placeholder="Search..."
+        value={search}
+      />
+      {isResultVisible && searchResult.length > 0 && (
+        <ui className="search-result">
+          {searchResult.map((search, index) => {
+            return <li key={index}>{search}</li>;
+          })}
+        </ui>
+      )}
+    </div>
+  );
 };
 
 export default AutoCompleteSearch;
--- src/JS/a4.js
+++ src/JS/a4.js
@@ -23,3 +23,38 @@ class ComputeAmount {
 
 console.log(new ComputeAmount().lacs(12).crore(5).crore(2).thousand(1).value());
 console.log(new ComputeAmount().crore(2).thousand(1).value());
+
+function handleSubmit() {
+  console.log("Api call");
+}
+
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+const throttle = (func, delay) => {
+  let flag = true;
+  return function (args) {
+    if (flag) {
+      func(args);
+      flag = false;
+
+      setInterval(() => {
+        flag = true;
+      }, delay);
+    }
+  };
+};
+
+const handleDebounceFunction = debounce(handleSubmit, 400);
+
+document.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  handleDebounceFunction();
+});
